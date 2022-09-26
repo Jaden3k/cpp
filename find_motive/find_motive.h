72fafcae0d616ccf956c6071ec420a0081e597e0
Jaden3K <romainschonholzer@epita.fr>
2022-09-26 23:25:39 +0200

    first push

--- find_motive/find_motive.h
+++ find_motive/find_motive.h
@@ -1,8 +1,7 @@
-//
-// Created by jaden on 26/09/22.
-//
+#pragma once
 
-#ifndef FIND_MOTIVE_FIND_MOTIVE_H
-#define FIND_MOTIVE_FIND_MOTIVE_H
+#include <iostream>
 
-#endif //FIND_MOTIVE_FIND_MOTIVE_H
+int find_motive(std::string path, std::string motive);
+int max(int a, int b);
+void __find_motive(std::string str, int size, int tmp[256]);
---
+++ find_motive/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <fstream>
+
+#include "find_motive.h"
+
+int main(int argc, char *argv[])
+{
+    if (argc != 3)
+    {
+        std::cout << "Error in number of arguments" << std::endl;
+        return 1;
+    }
+
+    std::string path = argv[1];
+    std::string pattern = argv[2];
+
+    std::ifstream file (path);
+
+    if (!file.is_open())
+    {
+        std::cout<<"The file " << path << " could not be opened." << std::endl;
+        return 1;
+    }
+
+    std::string str;
+    str = std::string((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
+
+    int count = find_motive(str, pattern);
+
+    std::cout<<"The file " << path << " contains "<< count << " words containing the motive " << pattern << std::endl;
+
+    return 0;
+}
